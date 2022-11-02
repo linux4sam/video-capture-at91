9bce302fb64fe7684d398a8effa8aeb87970310d
Eugen Hristev <eugen.hristev@microchip.com>
2022-11-02 17:20:39 +0200

    sama5d2: ov5640: replace RGGB with BGGR rotation
    It appears the sensor emits BGGR instead of RGGB at this moment.
    If it emits BGGR, then have this format used such that all elements
    in the pipeline are aware.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- sama5d2/ov5640/1280x720_raw.sh
+++ sama5d2/ov5640/1280x720_raw.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Preparing OV5640 in RAW BAYER MODE
-media-ctl -d /dev/media0 --set-v4l2 '4:0[fmt:SRGGB8_1X8/1280x720@1/30 field:none colorspace:srgb xfer:srgb ycbcr:601 quantization:full-range]'
-media-ctl -d /dev/media0 --set-v4l2 '"atmel_isc_scaler":0[fmt:SRGGB8_1X8/1280x720 field:none colorspace:srgb]'
+media-ctl -d /dev/media0 --set-v4l2 '4:0[fmt:SBGGR8_1X8/1280x720@1/30 field:none colorspace:srgb xfer:srgb ycbcr:601 quantization:full-range]'
+media-ctl -d /dev/media0 --set-v4l2 '"atmel_isc_scaler":0[fmt:SBGGR8_1X8/1280x720 field:none colorspace:srgb]'
 echo Ready to capture at 1280x720
 
