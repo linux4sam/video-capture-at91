204dece52dd32d9550d1f8b363af153b077f326e
Eugen Hristev <eugen.hristev@microchip.com>
2023-01-20 14:59:17 +0530

    sam9x75: imx219: add scripts
    Add scripts to configure media entities and bus formats. These scripts
    are useful to test camera interface
    
    Signed-off-by: Balamanikandan Gunasundar <balamanikandan.gunasundar@microchip.com>

---
+++ sam9x75/imx219/3264x2464.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+media-ctl -d /dev/media0 --set-v4l2 '"imx219 1-0010":0[fmt:SRGGB10_1X10/3280x2464]'
+media-ctl -d /dev/media0 --set-v4l2 '"dw-csi.0":0[fmt:SRGGB10_1X10/3280x2464]'
+media-ctl -d /dev/media0 --set-v4l2 '"csi2dc":0[fmt:SRGGB10_1X10/3280x2464]'
+media-ctl -d /dev/media0 --set-v4l2 '"atmel_isc_scaler":0[fmt:SRGGB10_1X10/3280x2464]'
+echo "Ready to capture at 3264x2464"
