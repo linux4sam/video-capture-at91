49a3fd6dc8063098ade0debfc1726d1b222857c7
Claudiu Beznea <claudiu.beznea@microchip.com>
2023-05-08 12:13:33 +0300

    keep backward compatiblity with old kernels
    ISC scaler name has been changed starting with kernel 6.2. Along with
    it commit 0204a23dc227 ("s/atmel_isc_scaler/microchip_isc_scaler") switched
    to new scaler name. This patch add support to keep backward compatibility
    of video-capture-at91 scripts with old kernels.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- sama5d2/ov5640/640x480_YUYV.sh
+++ sama5d2/ov5640/640x480_YUYV.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+. $(dirname "$0")/../../utils.sh
+
 echo Preparing OV5640 in YUYV 8bits mode
 media-ctl -d /dev/media0 --set-v4l2 '4:0[fmt:YUYV8_2X8/640x480@1/24 field:none colorspace:srgb]'
-media-ctl -d /dev/media0 --set-v4l2 '"microchip_isc_scaler":0[fmt:YUYV8_2X8/640x480 field:none colorspace:srgb]'
+media-ctl -d /dev/media0 --set-v4l2 "\"$(get_scaler_name)\":0[fmt:YUYV8_2X8/640x480 field:none colorspace:srgb]"
 echo Ready to capture at 640x480
---
+++ utils.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+version() {
+	echo "$@" | awk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }'
+}
+
+get_scaler_name() {
+	local kernel_version=$(uname -r | awk -F- '{print $1}')
+	local kernel_extraversion=$(uname -r | awk -F- '{print $2}')
+	local scaler=atmel_isc_scaler
+
+	if [ "$(version "${kernel_version}")" -ge "$(version "6.2.0")" ]; then
+		scaler=microchip_isc_scaler
+	elif [[ "$(version "${kernel_version}")" -ge "$(version "6.1.0")" ]] && \
+	     [[ "${kernel_extraversion}" == *"linux4microchip"* ]]; then
+		scaler=microchip_isc_scaler
+	fi
+
+	printf "%s" "${scaler}"
+}
